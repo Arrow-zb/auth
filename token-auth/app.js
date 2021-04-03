c97970cd5cb8ec5c5f919a908462c8e853b9a74c
bo.zhang <bo.zhang@aispeech.com>
2021-04-03 19:20:49 +0800

    feat: token-auth

--- token-auth/app.js
+++ token-auth/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const router = new Router();
 const jsonwebtoken = require('jsonwebtoken');
-const jwt = require('koa-jwt');
+const jwtAuth = require('koa-jwt');
 const SECRET = 'arrow-secret';
 
 const userCollection = {      // 模拟数据库，用户信息
@@ -14,31 +14,50 @@ const userCollection = {      // 模拟数据库，用户信息
 
 app.use(koaStatic("views"));
 app.use(bodyParser());
-app.use(jwt({ 
-  secret: SECRET,
-  audience: 'arrow.cn',
-  cookie: true 
-}).unless({    // 登录接口不需要验证
-  path: [/\/login/]
-}));
+
+app.use(function (ctx, next) {
+  return next().catch((err) => {
+    if (err.status === 401) {
+      ctx.body = {
+        code: "401",
+        msg: err.originalError ? err.originalError.message : err.message
+      };
+    } else {
+      throw err;
+    }
+  });
+});
 
 router.post('/login', (ctx, next) => {
   const { username, password } = ctx.request.body;
   if(userCollection[username] && userCollection[username] === password) {
     ctx.body = {
-      code: 200,
+      code: "0",
       msg: '登录成功',
       token: jsonwebtoken.sign(
         { username: username },  // 加密userToken
         SECRET,
         { expiresIn: '1h' }
       )
-  }
+    }
   }else {
-    ctx.status = 401;
-    ctx.body = "用户不存在或者密码错误！";
+    ctx.body = {
+      code: "401",
+      msg: '用户不存在或者密码错误！'
+    }
   }
 });
+
+router.get("/getUserInfo", jwtAuth({
+  secret: SECRET
+}), ctx => {
+  console.log(ctx.state.user);
+  ctx.body = {
+    code: "0",
+    msg: '获取成功',
+    data: ctx.state.user
+  }
+})
 
 app
   .use(router.routes())
